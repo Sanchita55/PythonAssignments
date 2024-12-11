1e3b9c422c85a0d5fd0d4b1ac6e653ee99837303
Sanchita55 <sanchita9509@gmail.com>
2024-12-11 12:49:07 +0530

    ques3.py

--- Assignment1/ques3.py
+++ Assignment1/ques3.py
@@ -1,4 +1,5 @@
-# Calculation of aggregate & percentage marks
+# If the marks obtained by a student in five different subjects are input through the keyboard, find out the aggregate marks and percentage marks obtained by the student. Assume that the maximum marks that can be obtained by a student in each subject is 100.
+
 sub1 = int(input('Enter the marks of sub1: '))
 sub2 = int(input('Enter the marks of sub2: '))
 sub3 = int(input('Enter the marks of sub3: '))
