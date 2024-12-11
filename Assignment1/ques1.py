3b4b2730eb67cda77930d7f1dde7f0913ddaa2a9
Sanchita55 <sanchita9509@gmail.com>
2024-12-11 12:37:41 +0530

    ques1.py

--- Assignment1/ques1.py
+++ Assignment1/ques1.py
@@ -1,4 +1,4 @@
-# Calculate Ramesh's gross salary
+# Ramesh's basic salary is input through the keyboard. His dearness allowance is 40% of basic salary, and house rent allowance is 20% of bsic salary. Write a program to calculate his gross salary.
 bp = float(input('Enter Basic Salary of Ramesh: '))
 da = 0.4*bp
 hra = 0.2*bp
